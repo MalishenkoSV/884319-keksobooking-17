eb2c4782bbdb360e7769c348791912822e578e59
Svetlana <lisalana15@gmail.com>
2019-06-14 13:58:50 +0300

    Исправила ошибки

--- js/main.js
+++ js/main.js
@@ -16,10 +16,11 @@ map.classList.remove('map--faded');
 var getRandomFromInterval = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
 /**
- * Создает рандомнщу число из массива
- * @param {array} arr -массив
- * @return arr
+ * Создает рандомное число из массива
+ * @param {array} arr -- массив
+ * @return {array} arr
  */
 var getRandomElementFromArray = function (arr) {
   return arr[getRandomFromInterval(0, arr.length - 1)];
@@ -30,12 +31,12 @@ var getRandomElementFromArray = function (arr) {
  * @param {object} k — число
  * @return {object} -- объект данных о объявлении
  */
-var createData = function (k) {
+var createData = function () {
   var x = getRandomFromInterval(0, MAP_WIDTH);
   var y = getRandomFromInterval(MAP_HEIGTH_MIN, MAP_HEIGTH_MAX);
   var advertData = {
     author: {
-      avatar: 'img/avatars/user0' + (k + 1) + '.png'
+      avatar: 'img/avatars/user0' + getRandomFromInterval(1, COUNT) + '.png'
     },
     offer: {
       type: getRandomElementFromArray(TYPES)
@@ -54,7 +55,7 @@ var createData = function (k) {
 
 var adverts = [];
 for (var i = 0; i < COUNT; i++) {
-  var advert = createData(advertData.author, advertData.offer, advertData.location);
+  var advert = createData(adverts[i]);
   adverts.push(advert);
 }
 
@@ -66,9 +67,9 @@ for (var i = 0; i < COUNT; i++) {
 
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var createMapPin = function (pinData) {
-  if (advertData) {
+  if (pinData) {
     var pinCloneElement = pinTemplate.cloneNode(true);
-    pinTemplate.style = 'left:' + advertData.location.x + 'px;top:' + advertData.location.y + 'px;';
+    pinTemplate.style = 'left:' + pinData.location.x + 'px;top:' + pinData.location.y + 'px;';
     pinTemplate.querySelector('img').src = pinData.author.avatar;
     pinTemplate.querySelector('img').alt = pinData.offer.avatar;
   }
