b297b47668e87f2f8571b6e5df5f1ffc79f553da
Svetlana <lisalana15@gmail.com>
2019-07-18 16:40:15 +0300

    ?

--- js/map.js
+++ js/map.js
@@ -19,17 +19,17 @@
   var setAddressCoords = function (x, y) {
     window.variables.formAdress.querySelector('#address').value = x + ', ' + y;
   };
-
   /**
    *  Деактивация страницы
-   *
    */
   var pins = [];
   var deactivatePage = function () {
     setAddressCoords(MAP_WIDTH / 2, MAP_HEIGTH_MAX / 2);
     window.variables.formAdress.reset();
     window.pin.resetActivePin();
-    window.map.card.remove();
+    if (window.map.card) {
+      window.map.card.remove();
+    }
     window.form.deactivateForm();
     pins.forEach(function (pin) {
       pin.remove();
@@ -52,20 +52,46 @@
     window.backend.load(onLoad, window.popup.onError);
   };
   /**
+   * updateMap - Перерисовывает элементы 'Метка объявления' на основе
+   * отфильтрованных объявлений.
+   *
+   */
+  var ads = [];
+  var updateMap = function () {
+    // Закрывает открытый элемeнт 'Карточка объявления'
+    if (window.map.card) {
+      window.map.card.close();
+    }
+    // Удаляет существующие элементы 'Метка объявления'
+    pins.forEach(function (pin) {
+      pin.remove();
+    });
+
+    // 'Перемешивает' массив объявлений случайным обазом, чтобы при выборе опции 'любой'
+    // отображались случайные элементы
+    ads = window.util.mixArray(fetchAds(adverts));
+
+    // Фильтрует объявления и создает массив отфильтрованных объявлений
+    var filteredAds = window.filter.filterAds(ads);
+
+    // Добавляет DOM-элементы 'Метка объявления' на страницу
+    window.pin.showPinOnMap(filteredAds(pins));
+  };
+  /**
    * Функция активации страницы при клике на главную метку
    */
   var activatePage = function () {
-    fetchAds();
+    updateMap();
     setAddressCoords(MAP_WIDTH / 2, MAP_HEIGTH / 2);
     window.form.activateForm();
     // Разрешает мультизагрузку файлов
     // photoChooser.multiple = 'multiple';
     mainPin.removeEventListener('mouseup', activatePage);
-
   };
   window.map = {
     activatePage: activatePage,
     deactivatePage: deactivatePage,
-    setAddressCoords: setAddressCoords
+    setAddressCoords: setAddressCoords,
+    updateMap: updateMap
   };
 })();
